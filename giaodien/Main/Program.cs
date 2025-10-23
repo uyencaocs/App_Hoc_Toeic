ec71703f6da048ef7857c419243c78fff627ac28
uyencaocs <86728679+uyencaocs@users.noreply.github.com>
2025-10-23 12:11:11 +0700

    Update Program.cs

--- giaodien/Main/Program.cs
+++ giaodien/Main/Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-<<<<<<<< HEAD:Program.cs
+
 namespace TestDauVao
 ========
 namespace Main
->>>>>>>> origin/trungdev:giaodien/Main/Program.cs
+
 {
     internal static class Program
     {
@@ -20,11 +20,11 @@ namespace Main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<<< HEAD:Program.cs
+
             Application.Run(new Test_Form());
 ========
             Application.Run(new frmMain());
->>>>>>>> origin/trungdev:giaodien/Main/Program.cs
+
         }
     }
 }
