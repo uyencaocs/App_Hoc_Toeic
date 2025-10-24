646183463384b47f347b6a2883bce5976ae2aef5
uyen cao <uyencao.cs@gmail.com>
2025-10-25 01:13:40 +0700

    logic cua lich su nguoi dung

---
+++ giaodien/Main/Logic/SessionManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Logic
+{
+    internal class SessionManager
+    {
+        public static int LoggedInUserId { get; set; } = 0;
+    }
+}
